f33b811ccce9eaf2366e57996d7b72d763b92f96
Salvin Suraj <salvinsuraj11@gmail.com>
2022-01-19 14:38:05 +0530

    Created Local Repo

---
+++ app/src/main/java/com/example/pdfreader/MainViewHolder.java
@@ -0,0 +1,26 @@
+package com.example.pdfreader;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainViewHolder extends RecyclerView.ViewHolder {
+
+    public TextView txtName;
+    public CardView cardView;
+
+
+    public MainViewHolder(View itemView)
+    {
+        super(itemView);
+
+
+        txtName = itemView.findViewById(R.id.pdf_txtName);
+        cardView = itemView.findViewById(R.id.pdf_cardView);
+
+    }
+
+
+}
